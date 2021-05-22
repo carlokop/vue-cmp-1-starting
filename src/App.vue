3d6e165dc0f4e23c3330cc044a4580ff15f0ba3f
Carlo <ch.kop.ck@gmail.com>
2021-05-22 10:59:05 +0200

    initial

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+    <div>
+        <the-header title="RememberMe"></the-header>
+        <stored-resources
+            :resources="storedResources"
+        ></stored-resources>
+    </div>
+</template>
+
+<script>
+import StoredResources from './components/learning-resources/StoredResources.vue';
+import TheHeader from './components/layouts/TheHeader';
+
+export default {
+    components: {
+        StoredResources,
+        TheHeader,
+    },
+    data() {
+        return  {
+            storedResources: [
+                {id: 'official-guide', title: 'Official Guide', description: 'The official VueJS documentation', link: 'https://vuejs.org'},
+                {id: 'google', title: 'Google', description: 'Learn to Google...', link: 'https://google.com'}
+            ]
+        }
+    }
+}
+</script>
+
+<style>
+    @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+
+    * {
+    box-sizing: border-box;
+    }
+
+    html {
+    font-family: 'Roboto', sans-serif;
+    }
+
+    body {
+    margin: 0;
+    }
+</style>
